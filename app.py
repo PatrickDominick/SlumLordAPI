325e3818941119baf95852552a10435b947cb642
PatrickDominick <pdneary@gmail.com>
2021-09-01 14:21:53 -0500

    backend running

--- app.py
+++ app.py
@@ -4,13 +4,103 @@ from flask_marshmallow import Marshmallow
 from flask_cors import CORS 
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "" 
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://fvvmgkizujgmwa:0517489606a39152159e991d08799f65e4afad8b69ac38cbe85a46a689074c9a@ec2-107-22-18-26.compute-1.amazonaws.com:5432/d5g32hr9gauiee"
+ 
 
 db = SQLAlchemy(app) 
 ma = Marshmallow(app) 
 CORS(app) 
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable=False, unique=True)
+    password = db.Column(db.String, nullable=False)
+    money = db.Column(db.Float, nullable=False)
+    # Avatar, high score, anything else?
+    tokens = db.relationship("Token", backref="user", cascade="all, delete, delete-orphan")
+    #whenever user gets deleted cascade will get rid of their info like tokens
 
+    def __init__(self, username, password, money=0):
+        self.username = username
+        self.password = password
+        self.money = money
+
+class Token(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+
+    def __init__(self, name, user_id):
+        self.name = name
+        self.user_id = user_id
+
+class TokenSchema(ma.Schema):
+    class Meta: 
+        fields = ("id", "name", "user_id")
+
+token_schema = TokenSchema()
+multiple_token_schema = TokenSchema(many=True)
+
+class UserSchema(ma.Schema):
+    class Meta: 
+        fields = ("id", "username", "password", "money", "tokens") #Usually do not put password in but studying incription tomorrow
+    tokens = ma.Nested(multiple_token_schema) #makes it so tokens can be jsonified because it is an object. Now it can be read
+
+user_schema = UserSchema()
+multiple_user_schema = UserSchema(many=True)
+
+@app.route("/user/add", methods=["POST"])
+def add_user():
+    #check to see if JSON data is being sent
+    if request.content_type != "application/json":
+        return jsonify("Error, Data must be sent as JSON FOOL!!!")
+
+    post_data = request.get_json()
+    username = post_data.get("username")
+    password = post_data.get("password")
+    money = post_data.get("money", 0)
+
+    new_record = User(username, password, money) #these are passed to constructor for class
+    db.session.add(new_record)
+    db.session.commit()
+
+    return jsonify(user_schema.dump(new_record))
+
+@app.route("/user/get", methods=["GET"])
+def get_all_users():
+    all_users = db.session.query(User).all()
+    return jsonify(multiple_user_schema.dump(all_users))
+
+@app.route("/user/get/<username>", methods=["GET"])
+def get_user(username):
+    user = db.session.query(User).filter(User.username == username).first()
+    return jsonify(user_schema.dump(user))
+
+@app.route("/token/add", methods=["POST"])
+def add_token():
+    #check to see if JSON data is being sent
+    if request.content_type != "application/json":
+        return jsonify("Error, Data must be sent as JSON FOOL!!!")
+
+    post_data = request.get_json()
+    name = post_data.get("name")
+    user_id = post_data.get("user_id")
+
+    new_record = Token(name, user_id) #these are passed to constructor for class
+    db.session.add(new_record)
+    db.session.commit()
+
+    return jsonify(token_schema.dump(new_record))
+
+@app.route("/token/get", methods=["GET"])
+def get_all_tokens():
+    all_tokens = db.session.query(Token).all()
+    return jsonify(multiple_token_schema.dump(all_tokens))
+
+@app.route("/token/get/<id>", methods=["GET"])
+def get_token(id):
+    token = db.session.query(Token).filter(Token.id == id).first()
+    return jsonify(token_schema.dump(token))
 
 
 if __name__ == "__main__":
